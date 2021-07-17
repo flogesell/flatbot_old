c0bee2e110c82771b8f8166b0eb0fad4dea904ec
Flo <fgesell@hotmail.de>
2021-07-17 22:13:58 +0200

    added router guard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import store from '../store';
+
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import Dashboard from "../views/Dashboard.vue";
@@ -16,73 +18,108 @@ import Cookies from "../views/Cookies.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/register",
-    name: "Register",
-    component: Register,
-  },
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    component: Dashboard,
-  },
-  {
-    path: "/statistics",
-    name: "Statistics",
-    component: Statistics,
-  },
-  {
-    path: "/party",
-    name: "Party",
-    component: Party,
-  },
-  {
-    path: "/Flat",
-    name: "Flat",
-    component: Flat,
-  },
-  {
-    path: "/profile",
-    name: "Profile",
-    component: Profile,
-  },
-  {
-    path: "/settings",
-    name: "Settings",
-    component: Settings,
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: About,
-  },
-  {
-    path: "/impressum",
-    name: "Impressum",
-    component: Impressum,
-  },
-  {
-    path: "/datasecurity",
-    name: "Datasecurity",
-    component: Datasecurity,
-  },
-  {
-    path: "/cookies",
-    name: "Cookies",
-    component: Cookies,
-  },
-  {
-    path: "/",
-    name: "Login",
-    component: Login,
-  },
-];
-
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes: [
+    {
+      path: "/register",
+      name: "Register",
+      component: Register,
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+    },
+    {
+      path: "/",
+      name: "Dashboard",
+      component: Dashboard,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/statistics",
+      name: "Statistics",
+      component: Statistics,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/party",
+      name: "Party",
+      component: Party,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/Flat",
+      name: "Flat",
+      component: Flat,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/profile",
+      name: "Profile",
+      component: Profile,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/settings",
+      name: "Settings",
+      component: Settings,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/about",
+      name: "About",
+      component: About,
+    },
+    {
+      path: "/impressum",
+      name: "Impressum",
+      component: Impressum,
+    },
+    {
+      path: "/datasecurity",
+      name: "Datasecurity",
+      component: Datasecurity,
+    },
+    {
+      path: "/cookies",
+      name: "Cookies",
+      component: Cookies,
+      meta: {
+        requiresAuth: false,
+      },
+    },
+    { 
+      path: '*',
+      redirect: '/' 
+    }
+  ]
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (store.getters.isAuthenticated) {
+      next()
+    } else {
+      next('/login')
+    }
+  } else {
+    next()
+  }
+})
 
 export default router;
