27e8fac9842c609ebff871d22a48abd327e24db0
Flo <fgesell@hotmail.de>
2021-07-17 22:14:06 +0200

    added login function

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -3,11 +3,11 @@
     <div class="container">
       <img class="flatbot-logo" src="@/assets/flatbot-logo.png">
       <span class="flatbot-desc ">The bot that keeps an overview of all your housing expenses at all times</span>
-      <form class="login-register-form">
+      <form class="login-register-form" @submit.prevent="login">
         <label for="email">Email</label>
-        <input id="email" type="email">
+        <input id="email" type="email" v-model="username">
         <label for="email">Passwort</label>
-        <input id="password" type="password">
+        <input id="password" type="password" v-model="password">
         <input id="submit" type="submit" value="Login">
       </form>
       <router-link class="route-link" to="/forgotpassword" tag="span">Passwort vergessen?</router-link>
@@ -24,10 +24,36 @@
 <script>
 // @ is an alias to /src
 //import HelloWorld from "@/components/HelloWorld.vue";
+import auth from '@/services/user.service.js';
 
 export default { 
   name: "Login",
   components: {},
+  data() {
+    return {
+      username: '',
+      password: '',
+      msg: ''
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const credentials = {
+          email: this.username,
+          password: this.password
+        };
+        const response = await auth.login(credentials);
+        this.msg = response.data;
+        const token = response.accessToken;
+        const user = response.account_data;
+        this.$store.dispatch('login', { token, user });
+        this.$router.push('/');
+      } catch (error) {
+        this.msg = error;
+      }
+    }
+  }
 };
 </script>
 
