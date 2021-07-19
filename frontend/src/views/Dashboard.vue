3afc2e1619a572010c3042970da827d5eff992d2
Flo <fgesell@hotmail.de>
2021-07-19 22:19:09 +0200

    added overview dashboard

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="dashboard">
-    <div class="übersicht">
-      <span>-110€</span>
+    <div class="quick-view">
+      <div class="flex-row center full-height">
+        <div class="flex-col grow" style="width: 50%; padding: 15px;"><span class="dashboard-desc">Du schuldest</span><span class="dashboard-money">- 40,33€</span></div>
+        <div class="flex-col grow" style="width: 50%; padding: 15px;"><span class="dashboard-desc">Gesamte Ausgaben</span><span class="dashboard-money">40,33€</span></div>
+      </div>
+      <div class="addBill">+</div>
     </div>
     <section class="content">
-      <h1 class="section-header">Juli</h1>
+      <div class="header-hr"><h1 style="margin-bottom: 0px;" class="section-header">Juli</h1><hr></div>
       <finanzeintrag
         v-for="finanzeintrag in finanzeinträge"
         :id="finanzeintrag.id"
@@ -49,9 +53,99 @@ export default {
           betrag: 10.77,
           date: Date.now(),
         },
+        {
+          id: "ef5d2090-8asdfasd-4746-b3sdgsdfgf0-efbd2f7fe63e",
+          name: "Liz",
+          description: "Netto",
+          betrag: 10.77,
+          date: Date.now(),
+        },
+        {
+          id: "efsdfgsdfg90-8a59-4746-b3afdf0-efbd2f7fe63e",
+          name: "Liz",
+          description: "Netto",
+          betrag: 10.77,
+          date: Date.now(),
+        },
+        {
+          id: "ef5d2090-8a59-4746-b3afdf0-efbsdgdsfg63e",
+          name: "Liz",
+          description: "Netto",
+          betrag: 10.77,
+          date: Date.now(),
+        },
+        {
+          id: "ef5d2090-8a59-4746-b3afdf0-efbd2f7fe63e",
+          name: "Liz",
+          description: "Netto",
+          betrag: 10.77,
+          date: Date.now(),
+        },
+        {
+          id: "ef5d2090-8a59-4746-b3afdf0-efbd2f7fe63e",
+          name: "Liz",
+          description: "Netto",
+          betrag: 10.77,
+          date: Date.now(),
+        },
+
       ],
     };
   },
   computed: {},
 };
 </script>
+
+<style lang="scss" scoped>
+.quick-view {
+  background: #219ed6;
+  height: 200px;
+  position: relative;
+}
+
+.addBill {
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  background: white;
+  position: absolute;
+  bottom: -30px;
+  left: 50%;
+  transform: translate(-50%);
+  -webkit-box-shadow: 0px 0px 10px 10pxrgba(0,0,0,0.15);
+  box-shadow: 0px 0px 10px 10px rgba(0,0,0,0.15);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 50px;
+  font-weight: 200;
+  cursor: pointer;
+}
+
+.header-hr {
+  display: flex;
+  align-items:center;
+  margin-bottom: 15px;
+
+  h1 {
+    min-width: 54px;
+  }
+
+  hr {
+    flex-grow: 1;
+    align-items: center;
+    margin-left: 15px;
+    border: 2px solid #219ed6;
+  }
+}
+
+.dashboard-desc {
+  font-size: 32px;
+  color: white;
+}
+
+.dashboard-money {
+  font-size: 2em;
+  color: white;
+}
+</style>
