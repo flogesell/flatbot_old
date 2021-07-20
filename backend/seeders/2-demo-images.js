fd8a087055378a77c48245095e0a9a942456b5be
Flo <fgesell@hotmail.de>
2021-07-20 13:27:13 +0200

    added demo images

--- backend/seeders/2-demo-images.js
+++ backend/seeders/2-demo-images.js
@@ -1,27 +1,34 @@
-const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+const path = require('path'); 
+
 'use strict';
+
 
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('images', [{
+    return queryInterface.bulkInsert('images', [
+    {
       id: 1,
-      name: 'flo-gesell.png',
-      data: 'gesell@example.com',
+      name: 'flo.JPG',
+      user_id: "0bba4745-9830-4d39-a686-74312bcfc936",
+      data: fs.readFileSync( path.join(__dirname, '../demo/flo.JPG') ),
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       id: 2,
-      name: 'elli.png',
-      data: 'gesell@example.com',
+      name: 'elli.jpg',
+      user_id: "ef5d2090-8a59-4746-b3a0-efbd2f7fe63e",
+      data: fs.readFileSync( path.join(__dirname, '../demo/elli.jpg') ),
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       id: 3,
       name: 'luis.png',
-      data: 'gesell@example.com',
+      user_id: "f3752d8c-4702-446a-84f4-110c76607c7b",
+      data: fs.readFileSync( path.join(__dirname, '../demo/luis.png') ),
       createdAt: new Date(),
       updatedAt: new Date()
     }
