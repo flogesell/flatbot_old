756268cd71cb00b7cd383a177f84053a4dab35f3
Flo <fgesell@hotmail.de>
2021-10-22 13:16:33 +0200

    changed import

---
+++ frontend/src/components/inviteCode.vue
@@ -0,0 +1,25 @@
+<template>
+  <VueQrcode :value="this.inviteId()" :foreground="foreground" :size="size" level="H" />
+</template>
+<script>
+import VueQrcode from 'vue-qrcode'
+
+  export default {
+    props: ["code","type"],
+    data() {
+      return {
+        baseURL: "http://localhost:8080",
+        size: 200,
+        foreground: "#219ed6",
+      }
+    },
+    methods: {
+      inviteId() {
+            return this.baseURL + "/invite/" + this.type + "/" + this.code;
+        }
+    },
+    components: {
+      VueQrcode,
+    },
+  }
+</script>
